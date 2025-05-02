cddda505ca8c12007c5a93b1a885e82f9226679b
Thomas Boehm Machado <thomas.b.machado@gmail.com>
2025-05-02 14:53:43 -0300

    deposit_uncomplete

--- src/app/pages/deposit.tsx
+++ src/app/pages/deposit.tsx
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+const denominations = [
+  {value: 2, label: "2 R$"},
+  {value: 5, label: "5 R$"},
+  {value: 10, label: "10 R$"},
+  {value: 20, label: "20 R$"},
+  {value: 50, label: "50 R$"},
+  {value: 100, label: "100 R$"},
+  {value: 200, label: "200 R$"},
+]
 const Depositar = () => {
   const navigate = useNavigate();
+  const [Quantity, SetQuantity] = useState<Record<number,number>>({});
+  const SaldoAtual = 0;
+  const Q2 =0;
+
 
   return (
-    <div>
-      <h1>Depósito</h1>
-      <p>Digite o valor para depositar.</p>
-
-      {/* Botão de voltar */}
-      <button onClick={() => navigate("/")}>
-        Voltar
-      </button>
-    </div>
+    <body>
+      <header>
+        <div className="logo">DEV BANK</div>
+        <div className="user-info">
+           Nome: Mark <br />
+           Agência: 0000<br />
+           Conta: 00000-0<br />
+          </div>
+      </header>
+      <div className = "main">
+        <label>
+          <div className="deposit">Saldo Atual: 000</div>
+          <div className= "quantity"> Quantidade Depositada: 000</div>
+          <div className="quantity"> Total: 000</div>
+        </label>
+      </div>
+      <button></button>
+    </body>
   );
 };
 
