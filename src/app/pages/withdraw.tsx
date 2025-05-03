b84aff57f0fa7ffe9b6a2f873a43cf9d8298e964
mdowney04 <124715720+mdowney04@users.noreply.github.com>
2025-05-03 19:33:45 -0300

    Integracao completa, falta estilizacao

--- src/app/pages/withdraw.tsx
+++ src/app/pages/withdraw.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { API_URL } from "../environment";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
